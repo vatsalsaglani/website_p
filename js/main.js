0514364e1291663610cac822ec6ab65c29c5b5f8
vatsalsaglani <saglanivatsal@gmail.com>
2018-12-31 11:05:50 +0530

    New commits.

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+var typed = new Typed('.element', {
+    strings: ["Hey, this is Vatsal Saglani. ^10000", "A Machine Learning Enthusiast, ^3500",
+        "a Native Application Developer,... ^2000", "a Full Stack Web Developer,... ^1300",
+        "and A Proud Gujarati! ^1800", "Vatsal Saglani."
+    ],
+    typeSpeed: 190,
+    showCursor: true,
+    autoInsertCss: true
+
+});
+
+var typedx = new Typed('.elementx', {
+    strings: ["Hey, this is Vatsal Saglani. ^10000", "A Machine Learning Enthusiast, ^3500",
+        "a Native Application Developer,... ^2000", "a Full Stack Web Developer,... ^1300",
+        "and A Proud Gujarati! ^1800", "Vatsal Saglani."
+    ],
+    typeSpeed: 190,
+    showCursor: true,
+    autoInsertCss: true
+})
